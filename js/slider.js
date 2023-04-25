0ce3529fb3ba08432b9496680f3be2b77088a1e3
AlekseyDibikov <79219724+Aleksey-Dibikov@users.noreply.github.com>
2023-04-26 00:07:46 +0300

    slide

--- js/slider.js
+++ js/slider.js
@@ -1,23 +1,63 @@
-const prevBtn = document.querySelector('[data-action = "prev"]');
-const nextBtn = document.querySelector('[data-action= "next"]');
-const valueElem = document.querySelector('.photo');
+// const prevBtn = document.querySelector('[data-action = "prev"]');
+// const nextBtn = document.querySelector('[data-action= "next"]');
+// const valueElem = document.querySelector('.photo');
 
-const slider = {
-    value: 0,
-    next() {
-      this.value += 1
-  },
-  prev() {
-    this.value -= 1;
+// const slider = {
+//     value: 0,
+//     next() {
+//       this.value += 1
+//   },
+//   prev() {
+//     this.value -= 1;
+//   }
+// };
+
+// nextBtn.addEventListener('click', function () {
+//   slider.next();
+//   valueElem.textContent = slider.value;
+// });
+
+// prevBtn.addEventListener('click', function () {
+//   slider.prev();
+//   valueElem.textContent = slider.value;
+// });
+
+const slider = document.querySelector(".photo");
+const slides = document.querySelectorAll(".slides");
+const button = document.querySelectorAll(".slide-btn");
+
+let current = 0;
+let prev = 4;
+let next = 1;
+
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+}
+
+const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+
+const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
+
+const gotoNum = number => {
+  current = number;
+  prev = current - 1;
+  next = current + 1;
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+    slides[i].classList.remove("prev");
+    slides[i].classList.remove("next");
   }
-};
 
-nextBtn.addEventListener('click', function () {
-  slider.next();
-  valueElem.textContent = slider.value;
-});
+  if (next == 4) {
+    next = 0;
+  }
 
-prevBtn.addEventListener('click', function () {
-  slider.prev();
-  valueElem.textContent = slider.value;
-});
+  if (prev == -1) {
+    prev = 4;
+  }
+
+  slides[current].classList.add("active");
+  slides[prev].classList.add("prev");
+  slides[next].classList.add("next");
+}
